3790fffd8f36e166b5acd64555a0ddfd647dc4a7
SalehAmrouni <samrouni101@gmail.com>
2026-08-05 11:30:39 -0600

    wire up admin stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
             {/* Standalone Game Logo (No text, no border, scaled up) */}
             <Link href="/" className="hover:opacity-80 transition-opacity">
               <img 
-                src="/logo.jpg" 
+                src="/logo.png" 
                 alt="COMMISSIONERS" 
                 className="h-16 w-auto object-contain"
               />
