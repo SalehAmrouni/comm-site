0ab44c194d09b8fc3a7077c888a34fffd7d1fe10
SalehAmrouni <samrouni101@gmail.com>
2026-08-05 11:37:21 -0600

    wire up admin stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
               <img 
                 src="/logo.png" 
                 alt="COMMISSIONERS" 
-                className="h-36 w-auto object-contain"
+                className="h-26 w-auto object-contain"
               />
             </Link>
 
